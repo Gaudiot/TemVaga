53531007ab4b6961356f68f301c1d612cca143d7
Gaudiot <victor.gaudiot@gmail.com>
2020-12-07 00:16:56 -0300

    request rides page static

--- gui-tem-vaga/src/app/requested-rides/requested-rides.component.ts
+++ gui-tem-vaga/src/app/requested-rides/requested-rides.component.ts
@@ -6,8 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./requested-rides.component.css']
 })
 export class RequestedRidesComponent implements OnInit {
+  rides = [
+    {driver: "Guta", id: 1, time: 13, destiny: "Boa viagem"},
+     {driver: "Elaine", id: 2, time: 8, destiny: "Olinda"},
+     {driver: "Victor", id: 3, time: 10, destiny: "Boa Viagem"}
+    ];
 
   constructor() { }
+
+  reset(){
+    this.rides = [
+      {driver: "Guta", id: 1, time: 13, destiny: "Boa viagem"},
+       {driver: "Elaine", id: 2, time: 8, destiny: "Olinda"},
+       {driver: "Victor", id: 3, time: 10, destiny: "Boa Viagem"}
+      ];
+  }
+
+  cancelRide(id){
+    const rideIndex = this.rides.findIndex(ride => ride.id === id);
+    if (rideIndex >= 0){
+      this.rides.splice(rideIndex, 1)
+    }
+  }
 
   ngOnInit(): void {
   }
